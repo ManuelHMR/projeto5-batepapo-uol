8c3664d4228bdce062945c307203f3df574eb75f
ManuelHMR <manuelhmrjf@gmail.com>
2022-02-11 15:29:44 -0300

    login html + css

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function openAside(){
-  document.querySelector("aside").setAttribute("style","right: 0px")
-  
+function openAside() {
+  document.querySelector('aside').setAttribute('style', 'right: 0px')
+  document.querySelector('.shadow').classList.remove('hidden')
 }
