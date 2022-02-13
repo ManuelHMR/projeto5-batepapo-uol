36b655d11c7ea556038b5fda5d76098d68a57a44
ManuelHMR <manuelhmrjf@gmail.com>
2022-02-13 20:47:15 -0300

    envio de mensagem

--- main.js
+++ main.js
@@ -3,46 +3,69 @@ const urlSTATUS = "https://mock-api.driven.com.br/api/v4/uol/participants ";
 const urlMESSAGES ="https://mock-api.driven.com.br/api/v4/uol/messages";
 let userName = document.querySelector('.inputName')
 let userObj
+let statusPromise
 let participantsPromise
+let messagePromise
+let apiMessagesObj
+let responseMessage = axios.get(urlMESSAGES)
+let displayedMessages = document.querySelector('.messages')
 function login() {
   userObj = {
     name: userName.value
   }
   participantsPromise = axios.post(urlPARTICIPANTS, userObj)
-  participantsPromise.then(loged)
+  participantsPromise.then(logged)
   participantsPromise.catch(loginError)
 }
-function loged(){
+function logged(){
   document.querySelector('.login_screen').classList.add('hidden')
+  document.querySelector('footer').classList.remove('hidden')
+  setInterval(loadMessages, 3000)
+  setInterval(stayLogged, 3000)
+  responseMessage.then(responseMessageReturn)
 }
-function loginError(){
-  alert(`Digite o seu nome!`)
+function loginError(loginErrorData){
+  console.log(loginErrorData)
+  alert(`Erro no login! Digite outro nome.`)
+}
+function stayLogged(){
+  userObj = {
+    name: userName.value
+  }
+  statusPromise = axios.post(urlSTATUS, userObj)
+  statusPromise.then()
+}
+function loadMessages(){
+  displayedMessages.innerHTML = ''
+  responseMessage = axios.get(urlMESSAGES)
+  responseMessage.then(responseMessageReturn)
 }
 function addMessage() {
   let newUserMessage = document.querySelector('.chat').value
-  newMessage(newUserMessage)
+  userObj = {
+    from: userName.value,
+    to: "Todos",
+    text: newUserMessage,
+    type: "message"
+  }
+  statusPromise = axios.post(urlMESSAGES, userObj)
+  statusPromise.then()
 }
 function newMessage(type, time, from, to, msg) {
-  let displayedMessages = document.querySelector('.messages')
   displayedMessages.innerHTML = displayedMessages.innerHTML + `<div class="singleMessage ${type}"><span><span class="timeStyle">(${time})</span> <span class="strong">${from}</span> para <span class="strong">${to}</span>: ${msg}</span></div>`;
 }
-
-let apiMessagesObj
-let responseMessage = axios.get(urlMESSAGES)
-responseMessage.then(responseMessageReturn)
 function responseMessageReturn(apiMessages){
   apiMessagesObj = apiMessages.data
   console.log(apiMessagesObj)
   displayAllMessages()
 }
-
 function displayAllMessages(){
   for(let i = 0; i < apiMessagesObj.length; i++){
     newMessage(apiMessagesObj[i].type, apiMessagesObj[i].time, apiMessagesObj[i].from, apiMessagesObj[i].to, apiMessagesObj[i].text)
   }
+  const messagesArr = document.querySelectorAll('.singleMessage');
+  messagesArr[messagesArr.length-1].scrollIntoView();
 }
-
-
 function openAside() {
   document.querySelector('aside').setAttribute('style', 'right: 0px')
   document.querySelector('.shadow').classList.remove('hidden')
